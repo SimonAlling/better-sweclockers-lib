7532f2dcbc1194e847a773e59ce0dd098f02f66b
Simon Alling <alling.simon@gmail.com>
2019-01-20 19:37:44 +0100

    Fix proofreading NBSP bug
    Before this commit, the writing rules were still defined so as to work
    with HTML content, but we have moved to working with text nodes instead,
    which don't have HTML entities like &nbsp;. This commit upgrades to a
    newer version of the writing rules that uses literal NBSPs instead of
    HTML entities.

--- __tests__/proofreading.test.ts
+++ __tests__/proofreading.test.ts
@@ -3,15 +3,15 @@ import { processText, processNodeWith } from "../src/proofreading";
 const processNode = processNodeWith(processText);
 
 it("can proofread some text", () => {
-  const text = "4 GHz eller 4&nbsp;GHz";
+  const text = "4 GHz eller 4\u00A0GHz";
   expect(processText(text)).toMatchInlineSnapshot(
-    `"4<pre class=\\"mistake\\" title=\\"Förslag: hårt mellanslag\\"> </pre>GHz eller 4<pre class=\\"verified\\"><pre class=\\"any\\">&nbsp;</pre></pre>GHz"`
+    `"4<pre class=\\"mistake\\" title=\\"Förslag: hårt mellanslag\\"> </pre>GHz eller 4<pre class=\\"verified\\"><pre class=\\"any\\">\u00A0</pre></pre>GHz"`
   );
 });
 
 it("can process a text node", () => {
   const p = document.createElement("p");
-  p.textContent = "4 GHz eller 4&nbsp;GHz";
+  p.innerHTML = "4 GHz eller 4&nbsp;GHz";
   processNode(p);
   expect(p.innerHTML).toMatchInlineSnapshot(
     `"<span>4<pre class=\\"mistake\\" title=\\"Förslag: hårt mellanslag\\"> </pre>GHz eller 4<pre class=\\"verified\\"><pre class=\\"any\\">&nbsp;</pre></pre>GHz</span>"`
